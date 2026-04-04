88b1a3c0eb7e0e525e12b59d1731fbd72fee1548
eimiii1 <philipjohn1627@gmail.com>
2026-04-05 00:15:04 +0800

    initial: set up the database

---
+++ src/lib/database.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+
+let cached = global.mongoose
+if (!cached) {
+    cached = global.mongoose = { conn: null, promise: null }
+}
+
+export async function connectDB() {
+    if (cached.conn) return cached.conn
+    if (!cached.promise) {
+        cached.promise = (await mongoose.connect(process.env.MONGO_URI)).then(m => m)
+    }
+
+    cached.conn = await cached.promise
+    return cached.conn
+}
