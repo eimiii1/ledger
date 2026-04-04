0c89977ebaaaf597d4b54979f7e275181afd7970
eimiii1 <philipjohn1627@gmail.com>
2026-04-05 00:51:18 +0800

    feat: set up register route

--- src/app/api/(auth)/register/route.js
+++ src/app/api/(auth)/register/route.js
@@ -1,7 +1,51 @@
 import { NextResponse } from "next/server";
 import { z } from 'zod'
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
+import User from "@/lib/models/User";
+import { connectDB } from "@/lib/database";
 
 const registerSchema = z.object({
     email_address: z.string(),
     password: z.string().min(8).regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)
 })
+
+export async function POST(request) {
+    try {
+        await connectDB()
+        const body = await request.json()
+        const result = registerSchema.safeParse(body)
+        if (!result.success) {
+            return NextResponse.json(
+                { message: result.error.issues },
+                { status: 400 }
+            )
+        }
+
+        // * if input validated -> process registration and create a token for instant login
+        const hashedPassword = await bcrypt.hash(result.data.password, 10)
+
+        const user = await User.create({
+            email_address: result.data.email_address,
+            password: hashedPassword
+        })
+
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET)
+        const response = NextResponse.json(
+            { success: true, userId: user._id },
+            { status: 201 }
+        )
+
+        response.cookies.set('token', token, {
+            httpOnly: true,
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        })
+        
+        return response
+    } catch (err) {
+        return NextResponse.json(
+            { message: err.message },
+            { status: 500 }
+        )
+    }
+}
--- src/lib/models/User.js
+++ src/lib/models/User.js
@@ -1,7 +1,12 @@
 import mongoose from 'mongoose'
 const userSchema = new mongoose.Schema({
     email_address: { type: String },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    username: { type: String },
+    birthday: { type: Date },
+    avatar: { type: String },
+    bio: { type: String },
+
 }, { timestamps: true })
 
 export default mongoose.models.User || mongoose.model('User', userSchema)
